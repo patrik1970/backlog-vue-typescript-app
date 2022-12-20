0ab0d873ee185786cd2a5ca0b40bef2a52f44164
Patrik Persson <Patrik.2.Persson@sigma.se>
2022-12-20 09:25:59 +0100

    Update table to fetch from api

--- src/components/IssueTable.vue
+++ src/components/IssueTable.vue
@@ -4,73 +4,68 @@
       <thead>
         <tr>
           <th class="text-left">
-            Name
+           Id
           </th>
           <th class="text-left">
-            Calories
+           Title
+          </th>
+          <th class="text-left">
+            Description
+          </th>
+          <th class="text-left">
+            Priority
+          </th>
+          <th class="text-left">
+            IssueType
+          </th>
+          <th class="text-left">
+            Created
+          </th>
+          <th class="text-left">
+            Completed
           </th>
         </tr>
       </thead>
       <tbody>
         <tr
-          v-for="item in desserts"
-          :key="item.name"
+          v-for="issue in issues"
+          :key="issue.id"
         >
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
+          <td>{{ issue.id }}</td>
+          <td>{{ issue.title }}</td>
+          <td>{{ issue.description }}</td>
+          <td>{{ issue.priority }}</td>
+          <td>{{ issue.issueType }}</td>
+          <td>{{ issue.created }}</td>
+          <td>{{ issue.completed }}</td>
         </tr>
       </tbody>
     </template>
   </v-simple-table>
 </template>
 
-<script>
-  export default {
-    data () {
-      return {
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
-      }
-    },
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import IssueDataService from "../services/IssueDataService";
+import Issue from "@/types/Issue";
+
+@Component
+export default class IssueTable extends Vue {
+  private issues: Issue[] = [];
+
+  fetchAllIssues() {
+    IssueDataService.getAll()
+      .then((response) => {
+        this.issues = response.data;
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
+
+  mounted() {
+    this.fetchAllIssues();
+  }
+}
 </script>
