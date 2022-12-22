e1f6c6ed5087b544eb7d2b40877928a2e16570db
Patrik Persson <Patrik.2.Persson@sigma.se>
2022-12-22 09:26:31 +0100

    Update textfields on IssueCard

--- src/components/IssueCard.vue
+++ src/components/IssueCard.vue
@@ -9,17 +9,22 @@
                     ></v-text-field>
                 </v-col>
                 <v-col>
-                    <v-text-field
+                    <v-select 
+                        :items="issueTypes"
+                        item-text="issueType"
+                        item-value="index"
                         label="IssueType"
                         outlined
-                    ></v-text-field>
+                    ></v-select>
                 </v-col>
-
                 <v-col>
-                    <v-text-field
+                    <v-select 
+                        :items="prioritys"
+                        item-text="priority"
+                        item-value="index"
                         label="Priority"
                         outlined
-                    ></v-text-field>
+                    ></v-select>
                 </v-col>
             </v-row>      
             <v-textarea
@@ -31,16 +36,92 @@
             ></v-textarea>
             <v-row  class="d-flex justify-space-between align-baseline">
                 <v-col>
-                    <v-text-field
-                        label="Created"
-                        outlined
-                    ></v-text-field>
+                    <v-menu
+                        ref="menu1"
+                        v-model="menu1"
+                        :close-on-content-click="false"
+                        :return-value.sync="date"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                                outlined
+                                v-model="date1"
+                                label="Created"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker
+                            v-model="date1"
+                            no-title
+                            scrollable
+                        >
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                text
+                                color="primary"
+                                @click="menu1 = false"
+                            >
+                                Cancel
+                            </v-btn>
+                            <v-btn
+                                text
+                                color="primary"
+                                @click="$refs.menu1.save(date)"
+                            >
+                                OK
+                            </v-btn>
+                        </v-date-picker>
+                    </v-menu> 
                 </v-col>
                 <v-col>
-                    <v-text-field
-                        label="Completed"
-                        outlined
-                    ></v-text-field>
+                    <v-menu
+                        ref="menu2"
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :return-value.sync="date"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                                outlined
+                                v-model="date2"
+                                label="Completed"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker
+                            v-model="date2"
+                            no-title
+                            scrollable
+                        >
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                text
+                                color="primary"
+                                @click="menu2 = false"
+                            >
+                                Cancel
+                            </v-btn>
+                            <v-btn
+                                text
+                                color="primary"
+                                @click="$refs.menu2.save(date)"
+                            >
+                                OK
+                            </v-btn>
+                        </v-date-picker>
+                    </v-menu>
                 </v-col>
                 <v-row
                     align="center"
@@ -71,3 +152,24 @@
         </v-container>   
     </v-card>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      issueTypes: [
+        { issueType: "Bug", index: '1' },
+        { issueType: "Feature", index: '2' },      
+        { issueType: "Documentation", index: '3' }
+      ],
+      prioritys: [
+        { priority: "Low", index: '1' },
+        { priority: "Medium", index: '2' },      
+        { priority: "High", index: '3' }  
+      ],
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu1: false,
+      modal: false,
+      menu2: false,
+    }),
+  }
+</script>
